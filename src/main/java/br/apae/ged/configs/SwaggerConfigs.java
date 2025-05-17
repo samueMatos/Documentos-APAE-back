f0608159cec7cd0cfda4dabf6df9b87e097e0277
CESAR AUGUSTO ALEXANDRE ALVES <cesar.alves@alunos.sc.senac.br>
2025-05-16 21:13:51 -0300

    back corrigido

--- src/main/java/br/apae/ged/configs/SwaggerConfigs.java
+++ src/main/java/br/apae/ged/configs/SwaggerConfigs.java
@@ -1,7 +1,10 @@
 package br.apae.ged.configs;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,13 @@ public class SwaggerConfigs {
                 .version("1.0")
                 .description("Esta API expõe endpoints para uso de um gerenciador de arquivos");
 
-        return new OpenAPI().info(information);
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                ))
+                .info(information);
     }
 }
--- src/main/java/br/apae/ged/repositories/UserRepository.java
+++ src/main/java/br/apae/ged/repositories/UserRepository.java
@@ -2,8 +2,9 @@ package br.apae.ged.repositories;
 
 import br.apae.ged.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
