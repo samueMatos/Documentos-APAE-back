574951afe6f04935ddaf7ba90deefb70863553ac
CESAR AUGUSTO ALEXANDRE ALVES <cesar.alves@alunos.sc.senac.br>
2025-07-03 20:35:01 -0300

    final

--- src/main/java/br/apae/ged/presentation/configs/TokenService.java
+++ src/main/java/br/apae/ged/presentation/configs/TokenService.java
@@ -1,11 +1,9 @@
 package br.apae.ged.presentation.configs;
 
 import br.apae.ged.domain.models.User;
-import br.apae.ged.domain.repositories.UserRepository;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -21,8 +19,6 @@ public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
 
-    private final UserRepository repository;
-
     public String generateToken(User user){
         try{
             Algorithm algorithm = Algorithm.HMAC256(secret);
@@ -37,16 +33,14 @@ public class TokenService {
     }
 
     public String validateToken(String token){
-        try {
-            Algorithm algorithm = Algorithm.HMAC256(secret);
-            return JWT.require(algorithm)
-                    .withIssuer("backend-app")
-                    .build()
-                    .verify(token)
-                    .getSubject();
-        } catch (JWTVerificationException exception){
-            return "";
-        }
+    
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        return JWT.require(algorithm)
+                .withIssuer("backend-app")
+                .build()
+                .verify(token)
+                .getSubject();
+        
     }
 
     private Instant genExpirationDate(){
